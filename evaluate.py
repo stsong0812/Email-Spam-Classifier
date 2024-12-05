ca9a695fc00b1087c156db5b6a550941bfa53f56
FirstoLiudi <first.l11101990@gmail.com>
2024-12-04 18:28:22 -0800

    refactor evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,35 +1,32 @@
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics import confusion_matrix,accuracy_score,precision_score
 import pandas as pd
 from joblib import load
+from sklearn.metrics import confusion_matrix,classification_report
 
-def evaluate_model(model:MultinomialNB,vectorizer:TfidfVectorizer,test_set_csv='cleaned_emails_test.csv'):
+# load test samples
+data = pd.read_csv('cleaned_emails_test.csv')
+x_unvectorized = data['content'].fillna('')
+y=data['label'].values
 
-    # load test samples and vectorize it
-    data = pd.read_csv(test_set_csv)
-    x_unvectorized = data['content'].fillna('')
+def evaluate_model(model,vectorizer,x_unvectorized,y):
+
+    # vectorize test samples
     x=vectorizer.transform(x_unvectorized)
-    y=data['label'].values
 
     # make predictions with model
     predictions=model.predict(x)
 
-    # calculate confusion matrix, accuracy and precision based on predictions made
+    # calculate confusion matrix, accuracy, precision, recall, etc
     matrix=confusion_matrix(y,predictions,labels=['spam','ham'])
-    accuracy=accuracy_score(y,predictions)
-    precision=precision_score(y,predictions,pos_label='spam')
 
     print(matrix)
-    print(f'accuracy: {accuracy} ({accuracy*100}%)')
-    print(f'precision(positive=spam): {precision} ({precision*100}%)\n')
+    print(classification_report(y,predictions,digits=4))
 
 trained_model=load('trained_model.joblib')
 trained_model_vectorizer=load('trained_model_vectorizer.joblib')
 print('evaluation for training.py:')
-evaluate_model(trained_model,trained_model_vectorizer)
+evaluate_model(trained_model,trained_model_vectorizer,x_unvectorized,y)
 
 cross_trained_model=load('cross_trained_model.joblib')
 cross_trained_model_vectorizer=load('cross_trained_model_vectorizer.joblib')
 print('evaluation for cross_training.py:')
-evaluate_model(cross_trained_model,cross_trained_model_vectorizer)
+evaluate_model(cross_trained_model,cross_trained_model_vectorizer,x_unvectorized,y)
